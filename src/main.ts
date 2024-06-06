143cd5e0b369d247d9a9699c99e3cd18c97f712c
Abby L <yu950713@gmail.com>
2024-06-07 01:29:35 +0800

    enable cors to public

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // this line opens the api to public; optionally we can configure which origins are allowed
   app.setGlobalPrefix('api');
   await app.listen(3000);
 }
