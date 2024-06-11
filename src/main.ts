04784a246a55fe4ae6450d87352116fc7bb7a321
Abby L <yu950713@gmail.com>
2024-06-11 18:37:25 +0800

    use mylogger globally

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MyLoggerService } from './my-logger/my-logger.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+  app.useLogger(app.get(MyLoggerService));
   app.enableCors(); // this line opens the api to public; optionally we can configure which origins are allowed
   app.setGlobalPrefix('api');
   await app.listen(3000);
